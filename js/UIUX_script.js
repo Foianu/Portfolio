9cbaf4d54f61d672b82a0e990deb2336601062b0
Foianu <valentina.foianu@gmail.com>
2024-04-15 14:58:53 +0300

    replaced images on UIUX pages created html/css pages for project2

--- js/UIUX_script.js
+++ js/UIUX_script.js
@@ -1,31 +1,27 @@
-function photoClicked(photo) {
+// function photoClicked(srcpath) {
 
-    const popupDiv = document.querySelector("#popup")
-    const popupOverlayDiv = document.querySelector("#popup-overlay")
- 
-    popupDiv.style.display = "flex"
-    popupOverlayDiv.style.display = "flex"
- 
-    const clickedImage = photo.querySelector("img")
-    const popupImage = document.querySelector("#popup img")
- 
-    popupImage.src = clickedImage.src
+//     const popupDiv = document.querySelector("#popup")
+//     const popupOverlayDiv = document.querySelector("#popup-overlay")
 
-    document.body.style.overflow = "hidden"
-    popupDiv.style.overflow = "auto"
+//     popupDiv.style.display = "flex"
+//     popupOverlayDiv.style.display = "flex"
  
- }
- 
- function hidePopup() {
- 
-    const popupDiv = document.querySelector("#popup")
-    const popupOverlayDiv = document.querySelector("#popup-overlay")
- 
-    popupDiv.style.display = "none"
-    popupOverlayDiv.style.display = "none"
+//     const clickedImage = photo.querySelector("img")
+//     const popupImage = document.querySelector("#popup img")
 
-    document.body.style.overflow = "auto"
-    popupDiv.style.overflow = "hidden"
- }
+//     document.body.style.overflow = "hidden"
+//     popupDiv.style.overflow = "auto"
+//  }
  
+//  function hidePopup() {
+ 
+//     const popupDiv = document.querySelector("#popup")
+//     const popupOverlayDiv = document.querySelector("#popup-overlay")
+ 
+//     popupDiv.style.display = "none"
+//     popupOverlayDiv.style.display = "none"
+
+//     document.body.style.overflow = "auto"
+//     popupDiv.style.overflow = "hidden"
+//  }
  
